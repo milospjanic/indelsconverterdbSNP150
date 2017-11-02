a7c8e7acb71ff449842caaa30b0bdbc15c0168cd
Milos Pjanic <pjanic.milos@gmail.com>
2017-11-02 13:52:41 -0700

    Update indelsconverterdbSNP147.sh

--- indelsconverterdbSNP147.sh
+++ indelsconverterdbSNP147.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FILE=~/chrStartEnd2rsID/snp150Common.bed
-DIR=~/indelsconverterdbSNP147
+FILE=~/indelsconverterdbSNP150/snp150Common.bed
+DIR=~/indelsconverterdbSNP150
 SNPS=$(pwd)/$1
 echo Proccesing file:
 echo $SNPS 
@@ -10,10 +10,10 @@ echo $SNPS
 
 if [ ! -d $DIR ]
 then
-mkdir ~/indelsconverterdbSNP147
+mkdir ~/indelsconverterdbSNP150
 fi
 
-cd ~/indelsconverterdbSNP147
+cd ~/indelsconverterdbSNP150
 
 #check if dbsnp file exists, if not, download from snp150Common table using mysql
 
@@ -46,12 +46,6 @@ cut -f3 $SNPS.indel > $SNPS.indel.col3
 sed -E 's/:/\t/g' $SNPS.indel.col3 | cut -f1-2 > $SNPS.indel.col3.short
 
 
-#parse input file into insertions, SNPs and simple deletions, large deletions
-
-#awk '$2 == $3 {print $0}' $1.mod2 > $1.mod2.insertions
-#awk '$3 == $2+1 {print $0}' $1.mod2 > $1.mod2.snp.plus.simple.deletions 
-#awk '{if ($3 != $2+1 && $2 != $3) print $0}' $1.mod2 > $1.mod2.large.deletions
-
 #find positions of snps from the input list by comparing to snpdb
 
 awk 'NR==FNR {h1[$1] = $1; h2[$2]=$2; h3[$1$2]=$4; h4[$1$2]=1; next} {if(h2[$2]==$2 && h1[$1]==$1 && h4[$1$2]==1) print h3[$1$2]"\t"$0}' snp147Common.bed.insertions $SNPS.indel.col3.short > $1.rsID.nohead.insertions
@@ -61,6 +55,7 @@ awk 'NR==FNR {h1[$1] = $1; h2[$2]=$2; h3[$1$2]=$4; h4[$1$2]=1; next} {if(h2[$2]=
 awk 'NR==FNR {h1[$1] = $1; h2[$3]=$3; h3[$1$3]=$4; h4[$1$3]=1; next} {if(h2[$2]==$2 && h1[$1]==$1 && h4[$1$2]==1) print h3[$1$2]"\t"$0}' snp147Common.bed.insertions $SNPS.indel.col3.short >> $1.rsID.nohead.insertions
 awk 'NR==FNR {h1[$1] = $1; h2[$3]=$3; h3[$1$3]=$4; h4[$1$3]=1; next} {if(h2[$2]==$2 && h1[$1]==$1 && h4[$1$2]==1) print h3[$1$2]"\t"$0}' snp147Common.bed.snp.plus.simple.deletions $SNPS.indel.col3.short >> $1.rsID.nohead.snp.plus.simple.deletions
 awk 'NR==FNR {h1[$1] = $1; h2[$3]=$3; h3[$1$3]=$4; h4[$1$3]=1; next} {if(h2[$2]==$2 && h1[$1]==$1 && h4[$1$2]==1) print h3[$1$2]"\t"$0}' snp147Common.bed.large.deletions $SNPS.indel.col3.short >> $1.rsID.nohead.large.deletions
+
 #merge insertions, SNPs and simple deletions, large deletions
 
 cat $1.rsID.nohead.insertions $1.rsID.nohead.snp.plus.simple.deletions $1.rsID.nohead.large.deletions > $1.rsID
